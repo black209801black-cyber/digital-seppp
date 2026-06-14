38506009b7e5fea4177b2fd006d1ae660e6a6501
cruel2013 <mistakez2015@gmail.com>
2026-06-14 23:40:35 +0330

    new
    nwe

---
+++ baseInfo.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(0);
+$botToken = '6476720333:AAFlNiIygo8E3Dlgm7Oxf4Wgbd2KP9M-JO0';
+$dbUserName = 'exellair_digitalinfernobot';
+$dbPassword = 'R@S0ulpr0gr4m3r';
+$dbName = 'exellair_digitalinfernobot';
+$botUrl = 'https://exella.ir/bot/';
+$admin = 7320207084;
+?>
